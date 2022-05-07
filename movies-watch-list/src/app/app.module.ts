109b70362fa91e972046468c5243d9e201109cf8
Roner <Roner@hotmail.se>
2022-05-07 14:22:29 +0200

    Added components for project

--- movies-watch-list/src/app/app.module.ts
+++ movies-watch-list/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolBarComponent } from './components/tool-bar/tool-bar.component';
+import { MoviesHomeComponent } from './components/movies-home/movies-home.component';
+import { AddMoviesComponent } from './components/add-movies/add-movies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolBarComponent,
+    MoviesHomeComponent,
+    AddMoviesComponent
   ],
   imports: [
     BrowserModule,
